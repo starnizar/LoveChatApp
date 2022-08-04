6a58b82fab450afe7ad7bb128ef8385c1a5b8071
starnizar <nizarincorp@gmail.com>
2022-08-04 16:45:10 +0400

    header added

--- App.tsx
+++ App.tsx
@@ -2,13 +2,24 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import LoginScreen from './src/screens/LoginScreen';
+import MainHeader from './src/components/main/header/MainHeader';
 
 const App = () => {
   const Stack = createNativeStackNavigator();
 
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator
+        screenOptions={{
+          header: ({back, options, navigation, route}) => (
+            <MainHeader
+              navigation={navigation}
+              back={back}
+              options={options}
+              route={route}
+            />
+          ),
+        }}>
         <Stack.Screen name={'Login'} component={LoginScreen} />
       </Stack.Navigator>
     </NavigationContainer>
---
+++ src/components/main/header/MainHeader.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import {View} from 'react-native';
+import {Appbar} from 'react-native-paper';
+
+const MainHeader = () => {
+  return <Appbar />;
+};
+
+export default MainHeader;

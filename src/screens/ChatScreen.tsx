117a31f151e0d9eb869c935cece630a95c323b5a
s.pishch <s.pisch@hqsoftwarelab.com>
2022-08-04 09:16:22 +0400

    screens added

---
+++ src/screens/ChatScreen.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {StyleSheet, View} from 'react-native';
+
+const ChatScreen = () => {
+  return <View style={styles.container} />;
+};
+
+export default ChatScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
---
+++ src/screens/LoginScreen.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {StyleSheet, View} from 'react-native';
+
+const LoginScreen = () => {
+  return <View style={styles.container} />;
+};
+
+export default LoginScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});

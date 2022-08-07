fdc57c4ccbd78e1cb7788222c35a937b7eaa721d
starnizar <nizarincorp@gmail.com>
2022-08-07 14:57:41 +0400

    updated header

--- src/components/main/header/MainHeader.tsx
+++ src/components/main/header/MainHeader.tsx
@@ -1,27 +1,52 @@
 import React from 'react';
-import { Appbar, DefaultTheme } from 'react-native-paper';
+import { Appbar } from 'react-native-paper';
 import { NativeStackHeaderProps } from '@react-navigation/native-stack';
+import { StyleSheet } from 'react-native';
+import { colors } from '../../../styles/colors';
 
 const MainHeader = (props: NativeStackHeaderProps) => {
   const { route, navigation } = props;
 
   return (
-    <Appbar.Header theme={DefaultTheme} dark={true}>
-      {route.name !== 'Login' && route.name !== 'Chat' && (
-        <Appbar.BackAction onPress={() => navigation.goBack()} />
-      )}
-      <Appbar.Content title={route.name} />
-      {route.name === 'Login' && (
-        <Appbar.Action
-          icon="account-plus"
-          onPress={() => navigation.navigate('Create account')}
-        />
-      )}
-      {route.name === 'Chat' && (
-        <Appbar.Action icon="logout" onPress={() => navigation.pop()} />
-      )}
+    <Appbar.Header style={styles.header}>
+      <>
+        {route.name !== 'Login' && route.name !== 'Chat' && (
+          <Appbar.BackAction onPress={() => navigation.goBack()} />
+        )}
+      </>
+
+      <>
+        <Appbar.Content titleStyle={styles.headerContent} title={route.name} />
+      </>
+
+      <>
+        {route.name === 'Login' && (
+          <>
+            <Appbar.Action
+              icon="account-plus"
+              onPress={() => navigation.navigate('Create account')}
+            />
+            <Appbar.Action
+              icon="counter"
+              onPress={() => navigation.navigate('Counter')}
+            />
+          </>
+        )}
+        {route.name === 'Chat' && (
+          <Appbar.Action icon="logout" onPress={() => navigation.pop()} />
+        )}
+      </>
     </Appbar.Header>
   );
 };
 
 export default MainHeader;
+
+const styles = StyleSheet.create({
+  header: {
+    backgroundColor: colors.light,
+  },
+  headerContent: {
+    color: colors.dark,
+  },
+});

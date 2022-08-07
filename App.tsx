eec5f0599ff182dccbcc8d080296160be830bdba
starnizar <nizarincorp@gmail.com>
2022-08-07 14:54:29 +0400

    added Counter to navigation

--- App.tsx
+++ App.tsx
@@ -8,6 +8,7 @@ import Login from './src/screens/Login';
 import MainHeader from './src/components/main/header/MainHeader';
 import Chat from './src/screens/Chat';
 import CreateAccount from './src/screens/CreateAccount';
+import Counter from './src/screens/Counter';
 
 const App = () => {
   const Stack = createNativeStackNavigator();
@@ -25,6 +26,7 @@ const App = () => {
           component={Chat}
         />
         <Stack.Screen name={'Create account'} component={CreateAccount} />
+        <Stack.Screen name={'Counter'} component={Counter} />
       </Stack.Navigator>
     </NavigationContainer>
   );

4ea9e9dd2c3963deb244353abed99d08b98afdb3
starnizar <nizarincorp@gmail.com>
2022-08-07 14:58:33 +0400

    counter state from redux

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 import React from 'react';
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
-import {Provider as PaperProvider} from 'react-native-paper';
+import { name as appName } from './app.json';
+import { Provider as PaperProvider } from 'react-native-paper';
+import { Provider } from 'react-redux';
+import { store } from './src/redux/store/store';
 
 const ProvidedApp = () => (
-  <PaperProvider>
-    <App />
-  </PaperProvider>
+  <Provider store={store}>
+    <PaperProvider>
+      <App />
+    </PaperProvider>
+  </Provider>
 );
 
 AppRegistry.registerComponent(appName, () => ProvidedApp);
--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -1,9 +1,11 @@
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 
+// Screen interfaces
 export interface ChatScreenProps {
   navigation: NativeStackNavigationProp<any>;
 }
-
 export interface LoginScreenProps {
   navigation: NativeStackNavigationProp<any>;
 }
+
+// Styles interfaces
